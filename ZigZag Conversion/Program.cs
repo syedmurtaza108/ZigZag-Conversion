ffb00072c16fccd8dda5fe4d6bd0706ee74dbd3d
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-10-21 12:47:34 +0500

    problem solved

--- ZigZag Conversion/Program.cs
+++ ZigZag Conversion/Program.cs
@@ -8,26 +8,31 @@ namespace ZigZag_Conversion
         static void Main(string[] args)
         {
             Console.WriteLine(Convert("PAYPALISHIRING", 3));
+            Console.WriteLine(Convert("PAYPALISHIRING", 4));
+            Console.WriteLine(Convert("A", 1));
         }
 
         static string Convert(string s, int numRows)
         {
+            if (numRows == 1) return s;
             int a = numRows + numRows - 1;
             Dictionary<int, string> list = new Dictionary<int, string>();
-            list.Add(1,s[0].ToString());
-            int row = 2;
+            list.Add(1, s[0].ToString());
+            int row = 1;
             string result = "";
-            for (int i = 1; i < s.Length; i++)
+            for (int i = 1, j = 1; i < s.Length; i++, j++)
             {
-                list[row] = list.GetValueOrDefault(row, "") + s[i];
-                if (row > (a / 2) + 1)
+                if (j > a / 2)
                     row--;
                 else row++;
-                if (row == a - 1)
+                list[row] = list.GetValueOrDefault(row, "") + s[i];
+
+                if (j == a - 1)
                 {
-                    row = 2;
-                    a += a;
+                    row = 1;
+                    j = 0;
                 }
+
             }
             for (int i = 1; i <= numRows; i++)
             {
